f206de08e7df7e230350a86921d78920f1e36f3f
shenggan <csg19971016@gmail.com>
2022-03-03 23:25:03 +0800

    refactor kernel implementation

--- fastfold/model/kernel/cuda_native/csrc/softmax_cuda.cpp
+++ fastfold/model/kernel/cuda_native/csrc/softmax_cuda.cpp
@@ -1,7 +1,7 @@
 #include <torch/extension.h>
 
 at::Tensor softmax(at::Tensor input, int rows, int cols);
-at::Tensor softmax_gradient(at::Tensor d_output, at::Tensor input, int rows, int cols);
+at::Tensor softmax_gradient(at::Tensor d_output, at::Tensor output, int rows, int cols);
 
 at::Tensor fused_scale_mask_softmax_forward(at::Tensor input, at::Tensor mask, int rows, int cols,
                                             float scale);
@@ -15,8 +15,8 @@ at::Tensor fused_scale_mask_bias_softmax_backward(at::Tensor d_output, at::Tenso
                                                   int cols, float scale);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("forward_affine", &softmax, "Softmax forward (CUDA)");
-    m.def("backward_affine", &softmax_gradient, "Softmax backward (CUDA)");
+    m.def("forward", &softmax, "Softmax forward (CUDA)");
+    m.def("backward", &softmax_gradient, "Softmax backward (CUDA)");
 
     m.def("fused_scale_mask_softmax_forward", &fused_scale_mask_softmax_forward,
           "Softmax forward (CUDA)");
