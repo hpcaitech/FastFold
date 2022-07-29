e0489f760af1dbcdc93fe6834aa6cfb99b1216c5
Shenggan <csg19971016@gmail.com>
2022-07-29 10:25:50 +0000

    add chunk for self_att and opm

--- fastfold/model/fastnn/__init__.py
+++ fastfold/model/fastnn/__init__.py
@@ -1,6 +1,6 @@
 from .msa import MSAStack
-from .ops import OutProductMean
+from .ops import OutProductMean, set_chunk_size
 from .triangle import PairStack
 from .evoformer import Evoformer
 
-__all__ = ['MSAStack', 'OutProductMean', 'PairStack', 'Evoformer']
+__all__ = ['MSAStack', 'OutProductMean', 'PairStack', 'Evoformer', 'set_chunk_size']
