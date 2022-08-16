1b5c6d868e451b63becea9a658c21e3d1c651042
lclgy <gyang_lu@foxmail.com>
2022-08-16 18:02:34 +0800

    update readme and workflow_dispatch

--- .github/workflows/build_fastfold_wheel.sh
+++ .github/workflows/build_fastfold_wheel.sh
@@ -6,6 +6,7 @@ cuda_version=${3}
 python_version=${4}
 
 git reset --hard HEAD
+mkdir -p ./all_dist
 source activate base
 conda create -n $python_version -y python=$python_version
 source activate $python_version
@@ -15,6 +16,7 @@ pip install ./$filename
 pip install numpy
 
 python setup.py bdist_wheel
+mv ./dist/* ./all_dist
 python setup.py clean
 conda deactivate
 conda env remove -n $python_version
