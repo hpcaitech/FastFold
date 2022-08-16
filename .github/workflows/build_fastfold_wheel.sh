911a8f1a814382c624ef3b04d5725f69aff006a3
lclgy <gyang_lu@foxmail.com>
2022-08-16 17:15:20 +0800

    update readme, update build way

--- .github/workflows/build_fastfold_wheel.sh
+++ .github/workflows/build_fastfold_wheel.sh
@@ -1,30 +1,18 @@
 #!/usr/bin/env bash
 
-method=${1}
-url=${2}
-filename=${3}
-cuda_version=${4}
-python_version=${5}
-torch_version=${6}
-flags=${@:7}
+url=${1}
+filename=${2}
+cuda_version=${3}
+python_version=${4}
 
 git reset --hard HEAD
 source activate base
 conda create -n $python_version -y python=$python_version
 source activate $python_version
 
-if [ $1 == "pip" ]
-then
-    wget -nc -q -O ./$filename $url
-    pip install ./$filename
-    pip install numpy
-elif [ $1 == 'conda' ]
-then
-    conda install pytorch==$torch_version cudatoolkit=$cuda_version $flags
-else
-    echo Invalid installation method
-    exit
-fi
+wget -nc -q -O ./$filename $url
+pip install ./$filename
+pip install numpy
 
 python setup.py bdist_wheel
 python setup.py clean
