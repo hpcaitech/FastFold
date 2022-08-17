59d9dab32c2e9c6b6512f68af1f41e66e0fbf9cf
Fazzie <1240419984@qq.com>
2022-08-17 17:23:45 +0800

    support data aligment workflow

---
+++ fastfold/workflow/factory/__init__.py
@@ -0,0 +1,7 @@
+from .task_factory import TaskFactory
+from .hhblits import HHBlitsFactory
+from .hhsearch import HHSearchFactory
+from .jackhmmer import JackHmmerFactory
+from .alphafold import AlphaFoldFactory
+from .amber_relax import AmberRelaxFactory
+from .hhfilter import HHfilterFactory
