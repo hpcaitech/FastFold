730098c71eb6fee3061713bfeb5bef705977d753
oahzxl <xuanlei.zhao@gmail.com>
2022-09-26 15:43:39 +0800

    restore change

--- fastfold/workflow/template/fastfold_data_workflow.py
+++ fastfold/workflow/template/fastfold_data_workflow.py
@@ -119,7 +119,7 @@ class FastFoldDataWorkFlow:
 
 
     def run(self, fasta_path: str, alignment_dir: str=None, storage_dir: str=None) -> None:
-        storage_dir = "file:///tmp/ray/" + os.getlogin() + "/workflow_data"
+        storage_dir = "file:///tmp/ray/lcmql/workflow_data"
         if storage_dir is not None:
             if not os.path.exists(storage_dir):
                 os.makedirs(storage_dir, exist_ok=True)
