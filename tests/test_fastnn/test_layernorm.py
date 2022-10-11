2d2393aba84e900137e621d8242e51339ea09d9d
oahzxl <xuanlei.zhao@gmail.com>
2022-10-11 22:44:39 +0800

    support test without triton

--- tests/test_fastnn/test_layernorm.py
+++ tests/test_fastnn/test_layernorm.py
@@ -1,6 +1,12 @@
 import torch
 from fastfold.model.fastnn.kernel import LayerNorm as FastLayerNorm
-from fastfold.model.fastnn.kernel.layer_norm import FusedLayerNormAffineFunction, LayerNormTritonFunc
+from fastfold.model.fastnn.kernel.layer_norm import FusedLayerNormAffineFunction
+
+triton = True
+try:
+    from fastfold.model.fastnn.kernel.layer_norm import LayerNormTritonFunc
+except:
+    triton = False
 
 
 def test_layernorm():
@@ -21,7 +27,8 @@ def test_layernorm():
             torch_module = torch.nn.LayerNorm(normalized_shape=dim_).to(device=test_device,
                                                                         dtype=dtype)
             fastnn_cuda_module = FastLayerNorm(normalized_shape=dim_).to(device=test_device, dtype=dtype)
-            fastnn_triton_module = FastLayerNorm(normalized_shape=dim_).to(device=test_device, dtype=dtype)
+            if triton:
+                fastnn_triton_module = FastLayerNorm(normalized_shape=dim_).to(device=test_device, dtype=dtype)
 
             # Forward
             torch_out = torch_module(sample_input)
@@ -31,16 +38,16 @@ def test_layernorm():
             forward_error = torch.max(torch.abs(torch_out - fastnn_cuda_out)).cpu().item()
             assert forward_error < tolerance_eps[dtype], f"Error when {shape} {dtype}"
             
-            fastnn_triton_out = LayerNormTritonFunc.apply(sample_input, fastnn_triton_module.normalized_shape, fastnn_triton_module.weight, 
-                                                          fastnn_triton_module.bias, fastnn_triton_module.eps)
-            forward_error = torch.max(torch.abs(torch_out - fastnn_triton_out)).cpu().item()
-            assert forward_error < tolerance_eps[dtype], f"Error when {shape} {dtype}"
+            if triton:
+                fastnn_triton_out = LayerNormTritonFunc.apply(sample_input, fastnn_triton_module.normalized_shape, fastnn_triton_module.weight, 
+                                                            fastnn_triton_module.bias, fastnn_triton_module.eps)
+                forward_error = torch.max(torch.abs(torch_out - fastnn_triton_out)).cpu().item()
+                assert forward_error < tolerance_eps[dtype], f"Error when {shape} {dtype}"
 
             # Backward
             out_grad = torch.rand_like(torch_out).requires_grad_(False)
             torch_out.backward(out_grad)
             fastnn_cuda_out.backward(out_grad)
-            fastnn_triton_out.backward(out_grad)
 
             backward_weight_error = torch.max(
                 torch.abs(torch_module.weight.grad - fastnn_cuda_module.weight.grad)).cpu().item()
@@ -49,12 +56,14 @@ def test_layernorm():
                 torch.abs(torch_module.bias.grad - fastnn_cuda_module.bias.grad)).cpu().item()
             assert backward_bias_error < tolerance_eps[dtype], f"Error when {shape} {dtype}"
 
-            backward_weight_error = torch.max(
-                torch.abs(torch_module.weight.grad - fastnn_triton_module.weight.grad)).cpu().item()
-            assert backward_weight_error < tolerance_eps[dtype], f"Error when {shape} {dtype}"
-            backward_bias_error = torch.max(
-                torch.abs(torch_module.bias.grad - fastnn_triton_module.bias.grad)).cpu().item()
-            assert backward_bias_error < tolerance_eps[dtype], f"Error when {shape} {dtype}"
+            if triton:
+                fastnn_triton_out.backward(out_grad)
+                backward_weight_error = torch.max(
+                    torch.abs(torch_module.weight.grad - fastnn_triton_module.weight.grad)).cpu().item()
+                assert backward_weight_error < tolerance_eps[dtype], f"Error when {shape} {dtype}"
+                backward_bias_error = torch.max(
+                    torch.abs(torch_module.bias.grad - fastnn_triton_module.bias.grad)).cpu().item()
+                assert backward_bias_error < tolerance_eps[dtype], f"Error when {shape} {dtype}"
 
 
 if __name__ == "__main__":
