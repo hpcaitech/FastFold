4cc35adb3d9d1d7eb12226bb5e47dd8fa0b8f914
oahzxl <xuanlei.zhao@gmail.com>
2022-10-19 22:14:27 +0800

    update inference and evoformer stack

--- tests/test_fastnn/test_evoformer_stack.py
+++ tests/test_fastnn/test_evoformer_stack.py
@@ -13,63 +13,69 @@ from fastfold.utils.import_weights import import_jax_weights_
 from fastfold.utils.test_utils import get_param_path
 
 
+@pytest.fixture(scope="module")
+def get_module_and_output():
+    with torch.no_grad():
+        config = model_config('model_1')
+        config.globals.inplace = False
+        model = AlphaFold(config)
+        import_jax_weights_(model, get_param_path())
+
+        fast_model = copy.deepcopy(model)
+        fast_model = inject_fastnn(fast_model)
+        fast_model = fast_model.evoformer
+        fast_model.eval()
+        
+        model = model.evoformer
+        model.eval().cuda()
+        
+        msa_len = 50
+        seq_len = 52
+        m = torch.randn((msa_len, seq_len, 256))
+        m_mask = torch.ones((msa_len, seq_len)).to(dtype=m.dtype)
+        z = torch.randn((seq_len, seq_len, 128))
+        z_mask = torch.ones((seq_len, seq_len)).to(dtype=z.dtype)
+        data = [m, z, m_mask, z_mask]
+        inputs = [copy.deepcopy(i).cuda() for i in data]
+        out = model(
+            *inputs, chunk_size=None, _mask_trans=config.model._mask_trans)
+    return fast_model, config, out, data
+
+
 @pytest.mark.parametrize('world_size', [1, 2])
-@pytest.mark.parametrize('chunk_size', [None, 3])
+@pytest.mark.parametrize('chunk_size', [None, 1])
 @pytest.mark.parametrize('inplace', [False, True])
-def test_state_dict(world_size, chunk_size, inplace):
-    config = model_config('model_1')
-    config.globals.chunk_size = chunk_size
-    config.globals.inplace = False
-    target_module = AlphaFold(config)
-    import_jax_weights_(target_module, get_param_path())
-
-    fast_module = copy.deepcopy(target_module)
-    fast_module = inject_fastnn(fast_module)
-
-    target_module = target_module.evoformer
-    fast_module = fast_module.evoformer
-    target_module = target_module.eval()
-    fast_module = fast_module.eval()
-    
+def test_state_dict(world_size, chunk_size, inplace, get_module_and_output):
     run_func = partial(_test_evoformer_stack, world_size=world_size, chunk_size=chunk_size, 
-                       inplace=inplace, fast_module=fast_module, target_module=target_module, config=config)
+                       inplace=inplace, get_module_and_output=get_module_and_output)
     mp.spawn(run_func, nprocs=world_size)
 
 
-def _test_evoformer_stack(rank, world_size, chunk_size, inplace, fast_module, target_module, config):
+def _test_evoformer_stack(rank, world_size, chunk_size, inplace, get_module_and_output):
     os.environ['RANK'] = str(rank)
     os.environ['LOCAL_RANK'] = str(rank)
     os.environ['WORLD_SIZE'] = str(world_size)
     # init distributed for Dynamic Axial Parallelism
     fastfold.distributed.init_dap()
 
-    target_module = target_module.cuda()
-    fast_module = fast_module.cuda()
-
-    msa_len = 80
-    seq_len = 64
-    m = torch.randn((msa_len, seq_len, 256)).cuda()
-    m_mask = torch.ones((msa_len, seq_len)).cuda().to(dtype=m.dtype)
-    z = torch.randn((seq_len, seq_len, 128)).cuda()
-    z_mask = torch.ones((seq_len, seq_len)).cuda().to(dtype=z.dtype)
+    fast_module, config, out, data = get_module_and_output
+    inputs = [copy.deepcopy(i).cuda() for i in data]
+    fast_module.cuda()
 
     with torch.no_grad():
-        m_out, z_out, s_out = target_module(
-        m, z, m_mask, z_mask, chunk_size=chunk_size, _mask_trans=config.model._mask_trans)
-
         set_chunk_size(chunk_size)
         if not inplace:
             m_fast, z_fast, s_fast = fast_module(
-                m, z, m_mask, z_mask, chunk_size=chunk_size, _mask_trans=config.model._mask_trans)
+                *inputs, chunk_size=chunk_size, _mask_trans=config.model._mask_trans)
         else:
             m_fast, z_fast, s_fast = fast_module.inplace(
-                [m], [z], m_mask, z_mask, chunk_size=chunk_size, _mask_trans=config.model._mask_trans)
+                [inputs[0]], [inputs[1]], inputs[2], inputs[3], chunk_size=chunk_size, _mask_trans=config.model._mask_trans)
             m_fast = m_fast[0]
             z_fast = z_fast[0]
 
-    error = torch.mean(torch.abs(m_out - m_fast))
+    error = torch.mean(torch.abs(out[0].cuda() - m_fast))
     assert error < 1e-3, f"Test m failed at chunk size: {chunk_size}, inplace: {inplace}. The position dif is {error}"
-    error = torch.mean(torch.abs(z_out - z_fast))
+    error = torch.mean(torch.abs(out[1].cuda() - z_fast))
     assert error < 1e-3, f"Test z failed at chunk size: {chunk_size}, inplace: {inplace}. The position dif is {error}"
-    error = torch.mean(torch.abs(s_out - s_fast))
+    error = torch.mean(torch.abs(out[2].cuda() - s_fast))
     assert error < 1e-3, f"Test s failed at chunk size: {chunk_size}, inplace: {inplace}. The position dif is {error}"
--- tests/test_inference.py
+++ tests/test_inference.py
@@ -16,40 +16,40 @@ from fastfold.utils.test_utils import get_data_path, get_param_path
 
 
 @pytest.fixture(scope="module")
-def get_module_and_output():
+def get_module_and_data():
     config = model_config('model_1')
     config.globals.inplace = False
     model = AlphaFold(config)
     import_jax_weights_(model, get_param_path())
     model.eval()
     model.cuda()
-    batch = pickle.load(open(get_data_path(), 'rb'))
-    batch = {k: torch.as_tensor(v).cuda() for k, v in batch.items()}
+    batch_cpu = pickle.load(open(get_data_path(), 'rb'))
+    batch = {k: torch.as_tensor(copy.deepcopy(v)).cuda() for k, v in batch_cpu.items()}
     with torch.no_grad():
         out = model(batch)
 
     fast_model = copy.deepcopy(model)
     fast_model = inject_fastnn(fast_model)
     fast_model.eval()
-    return fast_model, out
+    return fast_model, out, batch_cpu
 
 
 @pytest.mark.parametrize('world_size', [1, 2])
-@pytest.mark.parametrize('chunk_size', [None, 32])
+@pytest.mark.parametrize('chunk_size', [None, 1])
 @pytest.mark.parametrize('inplace', [False, True])
-def test_state_dict(world_size, chunk_size, inplace, get_module_and_output):
-    run_func = partial(_test_inference, world_size=world_size, chunk_size=chunk_size, inplace=inplace, get_module_and_output=get_module_and_output)
+def test_state_dict(world_size, chunk_size, inplace, get_module_and_data):
+    run_func = partial(_test_inference, world_size=world_size, chunk_size=chunk_size, inplace=inplace, get_module_and_data=get_module_and_data)
     mp.spawn(run_func, nprocs=world_size)
 
 
-def _test_inference(rank, world_size, chunk_size, inplace, get_module_and_output):
+def _test_inference(rank, world_size, chunk_size, inplace, get_module_and_data):
     os.environ['RANK'] = str(rank)
     os.environ['LOCAL_RANK'] = str(rank)
     os.environ['WORLD_SIZE'] = str(world_size)
     # init distributed for Dynamic Axial Parallelism
     fastfold.distributed.init_dap()
 
-    fast_model_, openfold_out = get_module_and_output
+    fast_model_, openfold_out, batch_ = get_module_and_data
 
     fast_model = copy.deepcopy(fast_model_)
     fast_model.cuda()
@@ -63,7 +63,7 @@ def _test_inference(rank, world_size, chunk_size, inplace, get_module_and_output
     fast_model.structure_module.lit_positions = fast_model.structure_module.lit_positions.cuda()
 
     set_chunk_size(fast_model.globals.chunk_size)
-    batch = pickle.load(open(get_data_path(), 'rb'))
+    batch = copy.deepcopy(batch_)
     batch = {k: torch.as_tensor(v).cuda() for k, v in batch.items()}
 
     with torch.no_grad():
