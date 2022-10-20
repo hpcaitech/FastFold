5ec63694be2305a53287c425f693ba4266025ca8
oahzxl <xuanlei.zhao@gmail.com>
2022-10-20 17:16:28 +0800

    update test

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -12,62 +12,64 @@ from fastfold.config import model_config
 from fastfold.model.fastnn import set_chunk_size
 from fastfold.utils.inject_fastnn import inject_fastnn
 from fastfold.utils.import_weights import import_jax_weights_
-from fastfold.utils.test_utils import get_data_path, get_param_path
 
 
 @pytest.fixture(scope="module")
-def get_module_and_data():
+def get_openfold_module_and_output():
     config = model_config('model_1')
     config.globals.inplace = False
     model = AlphaFold(config)
-    import_jax_weights_(model, get_param_path())
+    import_jax_weights_(model, '/data/scratch/fastfold/weight.npz')
     model.eval()
     model.cuda()
-    batch_cpu = pickle.load(open(get_data_path(), 'rb'))
-    batch = {k: torch.as_tensor(copy.deepcopy(v)).cuda() for k, v in batch_cpu.items()}
+    batch = pickle.load(open('/data/scratch/fastfold/mono_batch.pkl', 'rb'))
+    batch = {k: torch.as_tensor(v).cuda() for k, v in batch.items()}
     with torch.no_grad():
         out = model(batch)
-
-    fast_model = copy.deepcopy(model)
-    fast_model = inject_fastnn(fast_model)
-    fast_model.eval()
-    return fast_model, out, batch_cpu
+    return model, out
 
 
 @pytest.mark.parametrize('world_size', [1, 2])
-@pytest.mark.parametrize('chunk_size', [None, 1])
+@pytest.mark.parametrize('chunk_size', [None, 32])
 @pytest.mark.parametrize('inplace', [False, True])
-def test_state_dict(world_size, chunk_size, inplace, get_module_and_data):
-    run_func = partial(_test_inference, world_size=world_size, chunk_size=chunk_size, inplace=inplace, get_module_and_data=get_module_and_data)
+def test_state_dict(world_size, chunk_size, inplace, get_openfold_module_and_output):
+    run_func = partial(run_dist, world_size=world_size, chunk_size=chunk_size, inplace=inplace, model=get_openfold_module_and_output)
     mp.spawn(run_func, nprocs=world_size)
 
 
-def _test_inference(rank, world_size, chunk_size, inplace, get_module_and_data):
+def run_dist(rank, world_size, chunk_size, inplace, model):
     os.environ['RANK'] = str(rank)
     os.environ['LOCAL_RANK'] = str(rank)
     os.environ['WORLD_SIZE'] = str(world_size)
     # init distributed for Dynamic Axial Parallelism
     fastfold.distributed.init_dap()
+    inference(chunk_size, inplace, model)
 
-    fast_model_, openfold_out, batch_ = get_module_and_data
 
-    fast_model = copy.deepcopy(fast_model_)
-    fast_model.cuda()
 
-    fast_model.globals.chunk_size = chunk_size
-    fast_model.globals.inplace = inplace
+def inference(chunk_size, inplace, get_openfold_module_and_output):
 
-    fast_model.structure_module.default_frames = fast_model.structure_module.default_frames.cuda()
-    fast_model.structure_module.group_idx = fast_model.structure_module.group_idx.cuda()
-    fast_model.structure_module.atom_mask = fast_model.structure_module.atom_mask.cuda()
-    fast_model.structure_module.lit_positions = fast_model.structure_module.lit_positions.cuda()
+    model, out = get_openfold_module_and_output
 
-    set_chunk_size(fast_model.globals.chunk_size)
-    batch = copy.deepcopy(batch_)
+    model.globals.chunk_size = chunk_size
+    model.globals.inplace = inplace
+
+    fastmodel = copy.deepcopy(model)
+    fastmodel = inject_fastnn(fastmodel)
+    fastmodel.eval()
+    fastmodel.cuda()
+
+    fastmodel.structure_module.default_frames = fastmodel.structure_module.default_frames.cuda()
+    fastmodel.structure_module.group_idx = fastmodel.structure_module.group_idx.cuda()
+    fastmodel.structure_module.atom_mask = fastmodel.structure_module.atom_mask.cuda()
+    fastmodel.structure_module.lit_positions = fastmodel.structure_module.lit_positions.cuda()
+
+    set_chunk_size(model.globals.chunk_size)
+    batch = pickle.load(open('/data/scratch/fastfold/mono_batch.pkl', 'rb'))
     batch = {k: torch.as_tensor(v).cuda() for k, v in batch.items()}
 
     with torch.no_grad():
-        fast_out = fast_model(batch)
+        fastout = fastmodel(batch)
 
-    pos_dif = torch.max(torch.abs(fast_out["final_atom_positions"] - openfold_out["final_atom_positions"].cuda()))
+    pos_dif = torch.max(torch.abs(fastout["final_atom_positions"] - out["final_atom_positions"].cuda()))
     assert pos_dif < 5e-4, f"Test failed at chunk size: {chunk_size}, inplace: {inplace}. The position dif is {pos_dif}"
