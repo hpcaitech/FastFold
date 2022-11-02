d874736a8573616fc470f53dd167ace078f9dec0
oahzxl <xuanlei.zhao@gmail.com>
2022-11-02 20:39:06 +0800

    fix index

--- fastfold/model/fastnn/kernel/layer_norm.py
+++ fastfold/model/fastnn/kernel/layer_norm.py
@@ -38,10 +38,10 @@ class FusedLayerNorm(torch.nn.Module):
             out = torch.empty_like(input)
             chunk_size = int(4000 * 4000 / input.shape[-3])
             if len(input.shape) == 3:
-                for i in range(0, input.shape[-3], chunk_size):
+                for i in range(0, input.shape[-3]):
                     out[i:i + chunk_size] = self.kernel_forward(input[i:i + chunk_size])
             elif len(input.shape) == 4:
-                for j in range(0, input.shape[-4], chunk_size):               
+                for j in range(0, input.shape[-4]):               
                     for i in range(0, input.shape[-3], chunk_size):
                         out[j, i:i + chunk_size] = self.kernel_forward(input[j, i:i + chunk_size])
             else:
