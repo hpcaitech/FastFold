7e64f793da2e91bbf7c2a35bb61278539dcd1576
lclgy <gyang_lu@foxmail.com>
2022-12-01 20:12:05 +0800

    modify data module for train

--- fastfold/utils/tensor_utils.py
+++ fastfold/utils/tensor_utils.py
@@ -1,3 +1,4 @@
+# Copyright 2022 HPC-AI Tech Inc
 # Copyright 2021 AlQuraishi Laboratory
 # Copyright 2021 DeepMind Technologies Limited
 #
@@ -52,7 +53,8 @@ def dict_multimap(fn, dicts):
         if type(v) is dict:
             new_dict[k] = dict_multimap(fn, all_v)
         else:
-            new_dict[k] = fn(all_v)
+            # when bs = 1, returns [...] rather than [1, ...]
+            new_dict[k] = fn(all_v) if len(all_v) > 1 else all_v[0]
 
     return new_dict
 
