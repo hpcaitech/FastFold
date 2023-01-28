459bb6289744dccd547d208f833a79bdd4189090
lclgy <gyang_lu@foxmail.com>
2023-01-28 12:05:04 +0800

    add end to end unit test for training

--- fastfold/utils/test_utils.py
+++ fastfold/utils/test_utils.py
@@ -1,5 +1,8 @@
 import os
+import random
 
+import torch
+import numpy as np
 
 def get_param_path():
     # develop
@@ -15,3 +18,13 @@ def get_data_path():
         return '/home/lczxl/data2/fastfold/example_input/mono_batch.pkl'
     # test
     return '/data/scratch/fastfold/mono_batch.pkl'
+
+
+def set_seed(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
